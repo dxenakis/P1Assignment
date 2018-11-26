d40a6c279c7c38f7f6bc2e0a6de8f5e8bbd9e377
Argiris Stamatopoulos <argorrr@gmail.com>
2018-11-26 18:30:26 +0200

    Sorting Vehicles by Plate Number and minor corrections.

--- src/DAO/Vehicle.java
+++ src/DAO/Vehicle.java
@@ -1,52 +1,62 @@
 package DAO;
 
-public class Vehicle {
+public class Vehicle implements Comparable {
 
-    private int Id;
-    private String Manufacturer;
-    private String Model;
-    private String PlateNumber;
-    private int Type;           // Should be the Type of the Vehicle. For Example 1 for Car , 2 for Motorbike etc.
+    //private int id; Is the ID really needed? Plate Number is a unique String by itself.
+    private String manufacturer;
+    private String model;
+    private String plateNumber;
+    private int type;           // Should be the Type of the Vehicle. For Example 1 for Car , 2 for Motorbike etc.
 
-    public int getId() {
-        return Id;
-    }
+   /* public int getId() {
+        return id;
+    }*/
 
-    public void setId(int id) {
-        Id = id;
+    /*public void setId(int id) {
+        this.id = id;
+    }*/
+
+    public Vehicle(String manufacturer, String model, String plateNumber, int type) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.plateNumber = plateNumber;
+        this.type = type;
     }
 
     public String getManufacturer() {
-        return Manufacturer;
+        return manufacturer;
     }
 
     public void setManufacturer(String manufacturer) {
-        Manufacturer = manufacturer;
+        this.manufacturer = manufacturer;
     }
 
     public String getModel() {
-        return Model;
+        return model;
     }
 
     public void setModel(String model) {
-        Model = model;
+        this.model = model;
     }
 
     public String getPlateNumber() {
-        return PlateNumber;
+        return plateNumber;
     }
 
     public void setPlateNumber(String plateNumber) {
-        PlateNumber = plateNumber;
+        this.plateNumber = plateNumber;
     }
 
     public int getType() {
-        return Type;
+        return type;
     }
 
     public void setType(int type) {
-        Type = type;
+        this.type = type;
     }
 
-
+    @Override
+    public int compareTo(Object o) {
+        return getPlateNumber().toUpperCase().compareTo(((Vehicle)o).getPlateNumber().toUpperCase());
+    }
 }
--- src/MAIN/Main.java
+++ src/MAIN/Main.java
@@ -1,13 +1,6 @@
 package MAIN;
 
-import IO.Jdbc;
-import UTILITIES.CheckPlate;
-
 public class Main {
     public static void main(String[] args) {
-
-
-
-
     }
 }
