3087d65f60eba3ccb5ca544b3a48b559651cf2e8
dxenakis <dxenakis@outlook.com>
2018-11-28 21:43:07 +0200

    Commit For Pull

--- src/MAIN/Main.java
+++ src/MAIN/Main.java
@@ -1,8 +1,12 @@
 package MAIN;
+import java.io.FileNotFoundException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+
 
         }
+
     }
 
